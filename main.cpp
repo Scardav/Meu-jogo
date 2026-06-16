1a7b92a73167b319d1ea8f5d34e99069df62e240
DAVI BOLSONI <davi_bolsoni@estudante.sesisenai.org.br>
2026-06-16 15:04:40 -0300

    Novas falas do bem vindo.ajustamento de personagem

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main () {
-	cout << "=========Seja Bem Vindo A=============" << endl;
-	cout << "=========Coronhadas simulator=========" << endl;
+	string nomeHeroi;
+	
+		cout << "Seja Bem Vindo " << endl;
+		cout << "Masmorras Masmorras e mais Masmorras" << endl;
+		cout << " hum seja bem vindo... Diga me o seu nome caro aventureiro " << endl;
+		cout << "Digite seu nome abaixo\n";
+		cin >> nomeHeroi;
 }

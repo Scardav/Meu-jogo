eadcb3ba2625b40fff3442ebbaf72b1fd74ba81a
DAVI BOLSONI <davi_bolsoni@estudante.sesisenai.org.br>
2026-06-16 16:43:51 -0300

    novo menu do jogo com status

---
+++ Combate.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+	
+	string Nome;
+	int Vida, Ataque, Mana;
+	char e;
+	
+	
+	cout << "O e voce novamente... qual e seu nome mesmo";
+	cout << "fale para mim novamente seu nome porfavor: ";
+	cin >> Nome;
+	cout << "A sim seja Bem vindo novamente" << Nome;
+	
+	cout << "abra o menou para verificar seus status. Clique na tecla [e]";
+	cin >> e;
+}
--- main.cpp
+++ main.cpp
@@ -5,10 +5,47 @@ using namespace std;
 
 int main () {
 	string nomeHeroi;
+	char e, s;
 	
 		cout << "Seja Bem Vindo " << endl;
 		cout << "Masmorras Masmorras e mais Masmorras" << endl;
 		cout << " hum seja bem vindo... Diga me o seu nome caro aventureiro " << endl;
 		cout << "Digite seu nome abaixo\n";
 		cin >> nomeHeroi;
+		cout << "Seja bem vindo  " <<nomeHeroi;
+		
+		cout << "\nabra o menou para verificar seus status. Clique na tecla [e]";
+	cin >> e;
+		cout << "\n========================================================";
+		cout << "============================Menu==========================";
+		cout << "======                                            ========";
+		cout << "======                 Inventario                 ========";
+		cout << "======                                            ========";
+		cout << "=======______________________________________/ ========";
+		cout << "======                                            ========";
+		cout << "======                configuraçao                ========";
+		cout << "======                                            ========";
+		cout << "=======_______________________________________/========";
+		cout << "======                                            ========";
+		cout << "======                    status                  ========";
+		cout << "======                                            ========";
+		cout << "=======_______________________________________/========";
+		cout << "======                                            ========";
+		cout << "======                                            ========";
+		cout << "======                                           ========";
+		cout << "=======_______________________________________/========";
+		cout << "================================================================";
+		cout << "================================================================";
+		
+		cout << "clique [i] para invetario,\nclique [c]para configuraçoes\nclique [s]para ver seus status";
+		cin >> s;
+		
+		cout << "======STATUS======";
+		cout << "==    Vida:25    ==";
+		cout << "==   Ataque:75   ==";
+		cout << "==    Mana:5    ==";
+		cout << "===================";
+	
+	
+	return 0;
 }
